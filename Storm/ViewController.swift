ff17aaf9610f9e598bc1c127a34c9fb3e503c533
Michael Bardwell-Scott <Michael.Bardwell.Scott@Gmail.com>
2014-10-24 23:12:11 -0400

    Added NSData and NSURL to view controller to make a network call and retrieve data from web.

--- Storm/ViewController.swift
+++ Storm/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let apiKey = "bf28c6dd1744dae0174e81e060b43ac4"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        
+        let forecaseURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
+        
+        let weatherData = NSData.dataWithContentsOfURL(forecaseURL, options: nil, error: nil)
+        println(weatherData)
     }
 
     override func didReceiveMemoryWarning() {
