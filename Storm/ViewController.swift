8c0a41c2f33111f2dc187c5a082977b54f2a4d38
Michael Bardwell-Scott <Michael.Bardwell.Scott@Gmail.com>
2014-10-25 16:16:08 -0400

    Finished app. Added error message to handle networking errors and to return to app. Also added App and Launch Icons.

--- Storm/ViewController.swift
+++ Storm/ViewController.swift
@@ -58,6 +58,25 @@ class ViewController: UIViewController {
                     self.refreshActivityIndicator.hidden = true
                     self.refreshButton.hidden = false
                 })
+            } else {
+                
+                let networkIssueController = UIAlertController(title: "error", message: "Unable to connect to internet.", preferredStyle: .Alert)
+                
+                let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                networkIssueController.addAction(okButton)
+                
+                let cancelButton = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+                networkIssueController.addAction(cancelButton)
+                
+                self.presentViewController(networkIssueController, animated: true, completion: nil)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    
+                    //Stop refresh animation
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshActivityIndicator.hidden = true
+                    self.refreshButton.hidden = false
+                })
             }
             
         })
